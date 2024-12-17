3abf159a4bf64139e603722676cbc55bdf89dd05
validverify <seedindeserd@gmail.com>
2024-12-17 11:34:54 +0300

    Just init blank.

---
+++ src/crypto/__init__.py
@@ -0,0 +1,5 @@
+'''
+    Package that handles all cryptography operations like signing messages, \
+    creating dh and rsa keys for users and messages, encrypting and decrypting\
+     messages.
+'''
